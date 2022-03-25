e46b4edbc614151b6e5a69575cdf3a0fef75305e
sleekleek <sheriann.ngui@gmail.com>
2022-03-25 12:05:03 +0800

    Added frame skipping to split.py

--- split.py
+++ split.py
@@ -7,34 +7,44 @@ cap = cv2.VideoCapture('walking_sil.avi')
 
 try:
     if not os.path.exists('data'):
+        print("Creating '/data' directory..")
         os.makedirs('data')
+    else:
+        # already exists, clear
+        print("'/data' directory found, clearing..")
+        for file in os.listdir('data'):
+            os.remove(os.path.join('data', file))
+        print("'/data' cleared")
 except OSError:
-    print ('Error: Creating directory of data')
+    print('Error: Creating directory of data')
 
 currentFrame = 0
 count = 0
-save = 0
+totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+interval = int(totalFrames / 80)
+print("Total Frames: {}".format(totalFrames))
+print("Interval: {}".format(interval))
 
-ret, frame = cap.read()
+if cap.isOpened():
+    currentFrame = 0
+    while True:
+        ret, frame = cap.read()
 
-while ret and save < 80 :
-    # Capture frame-by-frame
+        if ret:
+            while currentFrame < 80:
+                # Capture frame-by-frame
 
-    ret, frame = cap.read()
-    # Saves image of the current frame in jpg file
-    name = './data/frame' + str(currentFrame) + '.png'
-    print ('Creating...' + name)
-    cv2.imwrite(name, frame)
+                ret, frame = cap.read()
+                # Saves image of the current frame in jpg file
+                name = './data/frame' + str(currentFrame) + '.png'
+                print('Creating...' + name)
+                cv2.imwrite(name, frame)
 
-
-    ret, frame = cap.read()
-
-    # To stop duplicate images
-    currentFrame += 1
-    count+=1
-    save+=1
-    
-
-# When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+                # To stop duplicate images
+                currentFrame += 1
+                count += interval
+                successful = cap.set(cv2.CAP_PROP_POS_FRAMES, count)
+                #print("cap set at {}: {}".format(count, successful))
+        cap.release()
+        break
+cv2.destroyAllWindows()
