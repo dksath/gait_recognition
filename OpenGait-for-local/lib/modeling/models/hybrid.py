c8413139cbe2a10051421f31a8e0034a91a0d2b5
Sathvik Krishna <sathvik_dharmapuri@mymail.sutd.edu.sg>
2022-06-03 10:46:07 +0800

    pushed hybrid.py

--- OpenGait-for-local/lib/modeling/models/hybrid.py
+++ OpenGait-for-local/lib/modeling/models/hybrid.py
@@ -64,6 +64,8 @@ class GaitSet(BaseModel):
 
         del ipts
         
+
+        #GaitSet
         outs = self.set_block1(sils)
         gl = self.set_pooling(outs, seqL, dim=1)[0]
         gl = self.gl_block2(gl)
